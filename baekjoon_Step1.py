0664e67f504643166e1a2b86aedfd7fbfb026ee3
yuunseo <dbstj7457@naver.com>
2023-01-04 10:46:09 +0900

    1단계는 모아서 해결

---
+++ baekjoon_Step1.py
@@ -0,0 +1,15 @@
+'''	10998번 문제
+A,B = input().split()
+print(int(A) * int(B))'''
+
+'''1008번 문제
+a,b = input().split()
+print(int(a)/int(b))'''
+
+'''10869번 문제'''
+a,b = input().split()
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)/int(b))
+print(int(a)%int(b))
