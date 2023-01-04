e66bc841958fe2c01993c5fa8c5fff056bb30077
yuunseo <dbstj7457@naver.com>
2023-01-04 10:54:27 +0900

    18108번 까지 해결

--- baekjoon_Step1.py
+++ baekjoon_Step1.py
@@ -6,10 +6,17 @@ print(int(A) * int(B))'''
 a,b = input().split()
 print(int(a)/int(b))'''
 
-'''10869번 문제'''
+'''10869번 문제
 a,b = input().split()
 print(int(a)+int(b))
 print(int(a)-int(b))
 print(int(a)*int(b))
 print(int(a)/int(b))
-print(int(a)%int(b))
+print(int(a)%int(b))'''
+
+'''10926번 문제
+name = input()
+print(name+"??!")'''
+
+'''18108번 문제'''
+print(int(input())-543)
