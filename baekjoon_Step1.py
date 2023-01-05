14f6e900e45bbd69e6d1f8a1f197cdd3d217e73e
yuunseo <dbstj7457@naver.com>
2023-01-05 13:16:53 +0900

    step1. 3003번 문제 해결

--- baekjoon_Step1.py
+++ baekjoon_Step1.py
@@ -18,5 +18,25 @@ print(int(a)%int(b))'''
 name = input()
 print(name+"??!")'''
 
-'''18108번 문제'''
-print(int(input())-543)
+'''18108번 문제
+print(int(input())-543)'''
+
+'''3003번 문제'''
+a,b,c,d,e,f = input().split()
+data = ['1','1','2','2','2','8']
+num = [a,b,c,d,e,f]
+answer = []
+
+Count = 0
+
+for i in data:
+    if(num[Count] == i):
+        answer.append(0)
+    else:
+        answer.append(int(i) - int(num[Count]))
+    Count += 1
+
+for i in answer:
+    print(i,end=' ')
+
+
