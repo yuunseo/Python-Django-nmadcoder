2f8e6391d2bcfe921e6fa69929674e951f982c1a
yuunseo <dbstj7457@naver.com>
2023-01-09 15:04:49 +0900

    3단계. 15552번

--- baekjoon_Step3.py
+++ baekjoon_Step3.py
@@ -18,7 +18,7 @@ for i in range(num-1,0,-1):
     num += i
 print(num)'''
 
-'''25304번'''
+'''25304번
 total = int(input())
 count = int(input())
 for i in range(count):
@@ -27,4 +27,17 @@ for i in range(count):
 if(total == 0):
     print("Yes")
 else:
-    print("No")
+    print("No")'''
+
+'''15552번'''
+import sys
+count=int(sys.stdin.readline())
+answer=[]
+for i in range(count):
+    num1, num2 = map(int,sys.stdin.readline().split())
+    answer.append(num1+num2)
+
+for i in answer:
+    print(i)
+
+
