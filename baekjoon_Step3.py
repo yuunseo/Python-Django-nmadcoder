3233f12971e52ac6f801aaa0cd0c641cc9645dc1
yuunseo <dbstj7457@naver.com>
2023-01-09 14:53:27 +0900

    3단계. 10950번

--- baekjoon_Step3.py
+++ baekjoon_Step3.py
@@ -1,5 +1,13 @@
-'''2739번'''
+'''2739번
 num = int(input())
 for i in range(1,10):
-    print(f"{num} * {i} = {num*i}")
-    
+    print(f"{num} * {i} = {num*i}")'''
+
+'''10950번'''
+count = int(input())
+answer=[]
+for i in range(count):
+    num1, num2 = input().split()
+    answer.append(int(num1)+int(num2))
+for i in answer:
+    print(i)
