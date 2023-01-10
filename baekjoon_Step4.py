cd9ac0a1e32a65bed12c4f4b24bb8be3ee17e266
yuunseo <dbstj7457@naver.com>
2023-01-11 08:46:12 +0900

    4단계. 10871번

--- baekjoon_Step4.py
+++ baekjoon_Step4.py
@@ -1,5 +1,13 @@
-'''10807번'''
+'''10807번
 Count =int(input())
 num=list(map(int,input().split(" ")))
 Q = int(input())
-print(num.count(Q))
+print(num.count(Q))'''
+
+'''10871번'''
+N, X=map(int,input().split(" "))
+num=list(map(int,input().split(" ")))
+for i in num:
+    if(X>i):
+        print(i,end=" ")
+        
