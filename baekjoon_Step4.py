9a6e0fd556baff76012ba52ddb4a6304be213164
yuunseo <dbstj7457@naver.com>
2023-01-11 08:52:48 +0900

    4단계. 2562번

--- baekjoon_Step4.py
+++ baekjoon_Step4.py
@@ -11,8 +11,17 @@ for i in num:
     if(X>i):
         print(i,end=" ")'''
 
-'''10818번'''
+'''10818번
 Count = int(input())
 num = list(map(int,input().split(" ")))
-print(min(num),max(num))
+print(min(num),max(num))'''
+
+'''2562번'''
+num=[]
+while(True):
+    num.append(int(input()))
+    if(len(num)==9):
+        break
+print(max(num),num.index(max(num))+1)
+
         
