448f06082744f1e056b40d6a271ecd126a73b3e4
yuunseo <dbstj7457@naver.com>
2023-07-11 18:41:29 +0900

    plus to models about Houses

--- Django/nomad-backend/houses/models.py
+++ Django/nomad-backend/houses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Houses(models.Model):
+
+    """Model Definition About Houses"""
+
+    name = models.CharField(max_length=140)
+    price = models.PositiveBigIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
