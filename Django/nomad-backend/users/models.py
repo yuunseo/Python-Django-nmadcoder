58b30aa89e23ca62038a3736cf0ba3ae30fe50e7
yuunseo <dbstj7457@naver.com>
2023-07-13 11:49:38 +0900

    new creating to user model & admin page

--- Django/nomad-backend/users/models.py
+++ Django/nomad-backend/users/models.py
@@ -1,9 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class User(AbstractUser):
-    first_name = models.CharField( max_length=150, editable=False)
-    last_name = models.CharField( max_length=150, editable=False)
-    name = models.CharField(max_length=150,default="")
-    is_host = models.BooleanField(default=False)
+    class GenderChoices(models.TextChoices):
+        MALE = (
+            "male",
+            "Male",
+        )
+        FEMALE = (
+            "female",
+            "Female",
+        )
+
+    class LanguageChoieces(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoieces(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
+    avatar = models.ImageField(
+        blank=True,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoieces.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoieces.choices,
+    )
