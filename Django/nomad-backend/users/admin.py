68b2d274dbc41d704aeda7c8dfabc46507b34dd3
yuunseo <dbstj7457@naver.com>
2023-07-13 10:35:33 +0900

    admin class custom

--- Django/nomad-backend/users/admin.py
+++ Django/nomad-backend/users/admin.py
@@ -5,4 +5,31 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        ("Profile",
+        {   "fields": (
+                    "username",
+                    "password",
+                    "name",
+                    "email",
+                    "is_host")
+            },
+        ),
+        ("Permissions",
+        {   "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",)
+            },
+        ),
+        ("Important Dates",
+         {"fields": ("last_login", "date_joined"),
+          "classes": ("collapse",)},
+         ),
+    )
+
+    
