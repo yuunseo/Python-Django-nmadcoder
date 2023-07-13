9128a20fa33ab20b3fbe27474ce2ca6dba9908d4
yuunseo <dbstj7457@naver.com>
2023-07-13 20:29:35 +0900

    6.7 Reviews

---
+++ Django/nomad-backend/reviews/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from common.models import CommonModel
+
+
+# Create your models here.
+class Review(CommonModel):
+
+    """Review from a User to a Room or Experience"""
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+
+    experiences = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    rooms = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    # room과 experience 한 가지만 했을 경우를 고려해, null과 blank 추가
+
+    payload = models.TextField()
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user} / ⭐{self.rating}"
