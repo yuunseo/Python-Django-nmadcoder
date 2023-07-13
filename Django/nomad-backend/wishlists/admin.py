e5aec994cf9ff3982049bb4b3b6d98104f6107b4
yuunseo <dbstj7457@naver.com>
2023-07-13 21:18:37 +0900

    6.8 Wishlists

---
+++ Django/nomad-backend/wishlists/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Wishlist
+
+
+# Register your models here.
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "user",
+        "created_at",
+        "updated_at",
+    )
