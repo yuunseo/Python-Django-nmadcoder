1b0e41fe6ab5de488735572d5b7852a8f407b624
yuunseo <dbstj7457@naver.com>
2023-07-13 12:24:46 +0900

    Room model, Amenity model 생성

--- Django/nomad-backend/rooms/models.py
+++ Django/nomad-backend/rooms/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+
+    """Room Model Definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = ("shared_room", "Shared Room")
+
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+    price = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    address = models.CharField(max_length=250)
+    pet_friendly = models.BooleanField(default=True)
+    kind = models.CharField(
+        max_length=20,
+        choices=RoomKindChoices,
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+
+
+class Amenity(models.Model):
+
+    """Amenity Definition"""
+
+    name = models.CharField(
+        max_length=150,
+    )
+    description = models.CharField(
+        max_length=150,
+        null=True,
+    )
