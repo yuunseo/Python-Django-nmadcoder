4574b6139b7dff5e24535955d57a08cc2119f9b2
yuunseo <dbstj7457@naver.com>
2023-07-14 13:40:25 +0900

    8.2 Actions

--- Django/nomad-backend/rooms/admin.py
+++ Django/nomad-backend/rooms/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero.")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 # Register your models here.
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
+
     list_display = (
         "name",
         "price",
