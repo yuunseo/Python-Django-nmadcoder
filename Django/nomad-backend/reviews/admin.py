035066fc7a3dd81983e24140c66863cdafcb5cb3
yuunseo <dbstj7457@naver.com>
2023-07-14 14:14:18 +0900

    8.3 Custom Filter

--- Django/nomad-backend/reviews/admin.py
+++ Django/nomad-backend/reviews/admin.py
@@ -1,5 +1,26 @@
+from typing import Any, Optional
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from .models import Review
+
+
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
 
 
 # Register your models here.
@@ -9,4 +30,9 @@ class ReviewAdmin(admin.ModelAdmin):
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "rooms__categoryy",
+    )
