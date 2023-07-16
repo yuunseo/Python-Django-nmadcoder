7286f341994bccbd5ab3ac806c8d14037b67cecf
yuunseo <dbstj7457@naver.com>
2023-07-16 15:06:11 +0900

    10.5 is_valid()

--- Django/nomad-backend/categories/serializers.py
+++ Django/nomad-backend/categories/serializers.py
@@ -2,5 +2,12 @@ from rest_framework import serializers
 
 
 class CategorySerializer(serializers.Serializer):
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
+    created_at = serializers.DateTimeField(read_only=True)
