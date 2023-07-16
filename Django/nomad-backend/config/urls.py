0baafa551006f48478325858b97979d61bd6663b
yuunseo <dbstj7457@naver.com>
2023-07-16 11:26:47 +0900

    9.0 Views (url과 functions)

--- Django/nomad-backend/config/urls.py
+++ Django/nomad-backend/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rooms.views import say_hello
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("rooms", say_hello),
 ]
