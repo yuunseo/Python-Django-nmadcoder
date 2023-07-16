3d95b0fb3921dfa46b44351076e4b1642927f86e
yuunseo <dbstj7457@naver.com>
2023-07-16 11:53:03 +0900

    9.2 URL Arguments

--- Django/nomad-backend/rooms/views.py
+++ Django/nomad-backend/rooms/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+def see_all_room(request):
+    return HttpResponse("See all Room")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"See {room_id} Room")
