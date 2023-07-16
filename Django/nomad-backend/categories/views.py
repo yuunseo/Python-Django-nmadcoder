e9548f34f35575cefb9bd7da084a96781c3eddee
yuunseo <dbstj7457@naver.com>
2023-07-16 13:35:19 +0900

    10.1 JSONResponse

--- Django/nomad-backend/categories/views.py
+++ Django/nomad-backend/categories/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category
+from django.http import JsonResponse
+
 
 # Create your views here.
+def categories(request):
+    all_categories = Category.objects.all()
+    # 우리는 user에게 HttpResponse를 전달하지 않을것.
+    # 우리는 JSON을 전달할거야.
+    return JsonResponse({"ok": True})
