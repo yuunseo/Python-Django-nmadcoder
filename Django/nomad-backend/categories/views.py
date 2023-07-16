0d0fbda7adb67688b87ad84af2af30d608df2f67
yuunseo <dbstj7457@naver.com>
2023-07-16 14:06:30 +0900

    10.3 Serializer by RestFramework

--- Django/nomad-backend/categories/views.py
+++ Django/nomad-backend/categories/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from .models import Category
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CategorySerializer
 
 
 # Create your views here.
 @api_view()
 def categories(request):
+    all_cate = Category.objects.all()
+    serializer = CategorySerializer(all_cate, many=True)
     return Response(
         {
             "ok": True,
-            "categoris": Category.objects.all(),
+            "categoris": serializer.data,
         }
     )
