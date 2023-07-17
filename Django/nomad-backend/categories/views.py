a898064275b38a4d1eeae7b349793947058668e2
yuunseo <dbstj7457@naver.com>
2023-07-17 17:47:11 +0900

    10.12 ModelViewSet - view를 간단하게 만들어주는 ModelViewSet에 대해

--- Django/nomad-backend/categories/views.py
+++ Django/nomad-backend/categories/views.py
@@ -6,47 +6,9 @@ from .serializers import CategorySerializer
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 
-class Categories(APIView):
-    def get(self, request):
-        all_cate = Category.objects.all()
-        serializer = CategorySerializer(all_cate, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(CategorySerializer(new_category).data)
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise NotFound
-
-    # self.get_object(pk)
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk),
-            data=request.data,
-            partial=True,
-        )
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(HTTP_204_NO_CONTENT)
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
