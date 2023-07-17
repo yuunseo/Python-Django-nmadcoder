2cbadabe400bc76e6b03574cbe0650abc59652a8
yuunseo <dbstj7457@naver.com>
2023-07-18 08:30:11 +0900

    11.2 Perks and PerkDetail

--- Django/nomad-backend/experiences/views.py
+++ Django/nomad-backend/experiences/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from .models import Perk
+from .serializers import PerkSerializer
+
 
 # Create your views here.
+class Perks(APIView):
+    def get(self, request):
+        all_perks = Perk.objects.all()
+        serializer = PerkSerializer(all_perks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PerkSerializer(data=request.data)
+        if serializer.is_valid():
+            new_perks = serializer.save()
+            return Response(PerkSerializer(new_perks).data)
+        else:
+            return Response(PerkSerializer.errors)
+
+
+class PerkDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Perk.objects.get(pk=pk)
+        except Perk.DoesNotExist:
+            return NotFound
+
+    def get(self, request, pk):
+        serializer = PerkSerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = PerkSerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_perk = serializer.save()
+            return Response(PerkSerializer(updated_perk).data)
+
+    def delete(self, request, pk):
+        d_perk = self.get_object(pk)
+        d_perk.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
