d7457cde856d9f3a285f418a91038653e8e7e54a
yuunseo <dbstj7457@naver.com>
2023-07-18 07:08:28 +0900

    11.0 All Amenities - Amenties API 만들기

--- Django/nomad-backend/rooms/views.py
+++ Django/nomad-backend/rooms/views.py
@@ -1,36 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .models import Amenity
+from .serializers import AmenitySerializer
 
 
-# Create your views here.
-def see_all_room(request):
-    rooms = Room.objects.all()
-    return render(
-        request,
-        "all_rooms.html",
-        {
-            "rooms": rooms,
-            "title": "hello this is title comes from django!",
-        },
-    )
+class Amenities(APIView):
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = (
+                serializer.save()
+            )  # .save()이후 장고는 new data가 저장된 amenities in new DB 반환
+            return Response(
+                AmenitySerializer(amenity).data
+            )  # 새롭게 업데이트 된 정보를 다시 JSON으로 내보내고 있다.
+        else:
+            return Response(serializer.errors)
 
 
-def see_one_room(request, room_pk):
-    try:
-        room = Room.objects.get(pk=room_pk)
-        return render(
-            request,
-            "room_details.html",
-            {
-                "room": room,
-            },
-        )
-    except Room.DoesNotExist:
-        return render(
-            request,
-            "room_details.html",
-            {
-                "not_found": True,
-            },
-        )
+class AmenityDetail(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
