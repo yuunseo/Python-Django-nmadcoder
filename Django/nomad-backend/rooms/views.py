43acb220f0ceb2779e8fc7b8f74cc093a6380c93
yuunseo <dbstj7457@naver.com>
2023-07-19 21:59:07 +0900

    11.15 File Uploads

--- Django/nomad-backend/rooms/views.py
+++ Django/nomad-backend/rooms/views.py
@@ -216,3 +216,8 @@ class RoomAmenities(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+
+class RoomPhotos(APIView):
+    def post(self, request, pk):
+        pass
