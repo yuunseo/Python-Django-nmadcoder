e3e15d46fa9e9d5b433a02e6f38544b17ef1baba
yuunseo <dbstj7457@naver.com>
2023-07-21 18:11:42 +0900

    15.5 JWT Decode

--- Django/nomad-backend/config/authentication.py
+++ Django/nomad-backend/config/authentication.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
+import jwt
 from users.models import User
 
 
@@ -17,3 +19,23 @@ class TrustMeBroAuthentication(BaseAuthentication):
 
 class TokenAuthentication(BaseAuthentication):
     pass
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.headers.get("jwt")
+        if not token:
+            return None
+        decoded = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=["HS256"],
+        )
+        pk = decoded.get("pk")
+        if not pk:
+            raise AuthenticationFailed("Invalid Token")
+        try:
+            user = User.objects.get(pk=pk)
+            return (user, None)
+        except User.DoesNotExist:
+            raise AuthenticationFailed("User not Found")
