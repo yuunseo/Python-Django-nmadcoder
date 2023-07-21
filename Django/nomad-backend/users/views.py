cc0e441e169b08b9e8f6952e76668aa69b3d57c3
yuunseo <dbstj7457@naver.com>
2023-07-21 13:53:21 +0900

    12.1 Create User

--- Django/nomad-backend/users/views.py
+++ Django/nomad-backend/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import exceptions
 from rest_framework.permissions import IsAuthenticated
 from . import serializers
 
@@ -24,6 +25,24 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise exceptions.ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
