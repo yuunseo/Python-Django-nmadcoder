f838b3290697aee4350ba5359356daf4cdbc28d1
yuunseo <dbstj7457@naver.com>
2023-07-21 14:23:32 +0900

    12.2 Change Password

--- Django/nomad-backend/users/views.py
+++ Django/nomad-backend/users/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework import exceptions
 from rest_framework.permissions import IsAuthenticated
 from . import serializers
+from .models import User
 
 
 # Create your views here.
@@ -47,3 +48,30 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise exceptions.NotFound
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise exceptions.ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            raise exceptions.ParseError
